da4041f284f80bec64a543b25775c6fe95e49e8f
Rabbi Hossen <joyahmedauthor@gmail.com>
2025-05-25 10:09:15 +0600

    Addition Subtraction Multiplication Division Modulus Floor Division

---
+++ day1.py
@@ -0,0 +1,22 @@
+# Introduction to Python & Installation
+# -------------------------------------
+# Python is a high-level, interpreted programming language known for its readability and simplicity.
+# It's widely used in web development, data science, automation, artificial intelligence, and more.
+# Python code is easy to write and understand, making it an excellent choice for beginners.
+
+# To install Python:
+# 1. Go to https://www.python.org/downloads/
+# 2. Download the latest version for your operating system.
+# 3. Follow the installation instructions and make sure to check "Add Python to PATH" during setup.
+
+# Now, let's start:
+
+# A traditional way to begin something meaningful — "In the name of Allah"
+print("Bismillah")
+
+
+# Ask the user for their name and display a personalized welcome message
+name = input("Enter your name: ")  
+print("Welcome to Python, " + name + "!")   
+
+ 
---
+++ day2.py
@@ -0,0 +1,20 @@
+# Operators & Conditional Logic
+
+# 1: Addition
+print(5 + 3) 
+
+# 2: Subtraction
+print(9 - 4)
+
+# 3: Multiplication
+print(6 * 10)
+
+# 4: Division
+print(8 / 2)
+
+# 5: Modulus
+print(10 % 3)
+
+#6: Floor Division
+print(9 // 2)
+
